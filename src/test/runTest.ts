0339946d81e059f33fb15325ebaa477a24f81598
Mazen <mazenbesher@users.noreply.github.com>
2023-12-03 11:36:28 +0100

    Fix long sock path for  testing

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -13,10 +13,17 @@ async function main() {
     // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
+    // Path to sample project as first argument, the launched VS Code instance will open it
+    const sampleProjectPath = path.resolve(__dirname, "../../sample-projects");
+
+    // Solve the issue IPC handle sock is longer than 103 chars
+    const userDataDir = ["--user-data-dir", `${os.tmpdir()}`];
+
     // Download VS Code, unzip it and run the integration test
     await runTests({
       extensionDevelopmentPath,
       extensionTestsPath,
+      launchArgs: [sampleProjectPath, ...userDataDir],
     });
   } catch (err) {
     console.error("Failed to run tests", err);
